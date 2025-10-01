e3b96038e8cbc109f6881502dadc671c802a9ed3
arnovr <arnovanrossum1@gmail.com>
2025-10-01 13:03:34 +0200

    Corrected the logo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {default as SecondaryButton} from "./buttons/SecondaryButton";
 export {default as PrimaryButton} from "./buttons/PrimaryButton";
 export {default as Footer} from "./layout/Footer";
+export {default as Logo} from "./layout/logo/Logo";
